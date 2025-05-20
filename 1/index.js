6ed81038101eefffa9f58287f69efe1f5c43729c
Aditya <anupama12sg@gmail.com>
2025-05-20 23:50:40 +0530

    feat: random button for colour flipper done

--- 1/index.js
+++ 1/index.js
@@ -1,6 +1,15 @@
 const body = document.getElementsByTagName("body")[0]
 
 function setColour(name) {
-    body.style.backgroundColor= name;
+    body.style.backgroundColor = name;
+}
+
+function randomColour() {
+    const green = Math.round(Math.random() * 255)
+    const red = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const colour = `rgb(${green}, ${red}, ${blue})`
+    body.style.backgroundColor = colour;
 }
 
