d3b085b53e4ce9e400b94b581c292d5fecab9c61
Aditya <anupama12sg@gmail.com>
2025-05-25 20:55:19 +0530

    chore: added quotes

---
+++ 3. Random Quote Generator/index.js
@@ -0,0 +1,37 @@
+const quotes = [
+    "Discipline is the bridge between goals and accomplishment. – Jim Rohn",
+
+    "Success doesn't come from what you do occasionally, it comes from what you do consistently. – Marie Forleo",
+
+    "We must all suffer one of two things: the pain of discipline or the pain of regret. -Jim Rohn",
+
+    "Discipline is choosing between what you want now and what you want most. – Abraham Lincoln (attributed)",
+
+    "The more disciplined you become, the easier life gets. – Steve Pavlina",
+
+    "There are no secrets to success. It is the result of preparation, hard work, and learning from failure. – Colin Powell",
+
+    "I’m a greater believer in luck, and I find the harder I work the more I have of it. – Thomas Jefferson",
+
+    "Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing. – Pelé",
+
+    "Without hard work, nothing grows but weeds. – Gordon B. Hinckley",
+
+    "Work hard in silence, let your success be your noise. – Frank Ocean",
+
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
+
+    "Don’t aim for success if you want it; just do what you love and believe in, and it will come naturally. – David Frost",
+
+    "Success usually comes to those who are too busy to be looking for it. – Henry David Thoreau",
+
+    "Opportunities don't happen. You create them. - Chris Grosser",
+
+    "The only place where success comes before work is in the dictionary. – Vidal ",
+
+    "Success is not final, failure is not fatal. It is the courage to continue that counts. – Winston Churchill",
+
+    "You are the master of your dreams and the creator of your realities.",
+
+    "You are not just a part of the universe — you are happening to the universe too."
+]
