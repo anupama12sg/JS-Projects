00d590f6979671e5aea805acd7b7043c491842ca
Aditya <anupama12sg@gmail.com>
2025-05-28 22:18:54 +0530

    chore: timer function called every 1000 seconds

--- 4. Stop watch/index.js
+++ 4. Stop watch/index.js
@@ -1,9 +1,13 @@
 let secondsElapsed = 0;
 let interval = null;
-const time =document.getElementById("time")
+const time = document.getElementById("time")
+
+function timer() {
+    secondsElapsed++;
+}
 
 function startClock() {
-
+    interval = setInterval(timer, 1000)
 }
 
 function stopClock() {
