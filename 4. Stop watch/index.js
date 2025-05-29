647c62140ea88e1c5c0d58d114db0c1974942c4e
Aditya <anupama12sg@gmail.com>
2025-05-29 23:13:16 +0530

    chore: added some style to the buttons

--- 4. Stop watch/index.js
+++ 4. Stop watch/index.js
@@ -8,9 +8,9 @@ function padStart(value) {
 
 
 function setTime() {
-    const minutes = Math.floor(secondsElapsed/60)
+    const minutes = Math.floor(secondsElapsed / 60)
     const seconds = (secondsElapsed % 60)
-    time.innerHTML = `${padStart(minutes)}:{padStart(seconds)}`;
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
 }
 
 function timer() {
@@ -19,13 +19,16 @@ function timer() {
 }
 
 function startClock() {
+    if (interval) resetClock()
     interval = setInterval(timer, 1000)
 }
 
 function stopClock() {
-
+    clearInterval(interval)
 }
 
 function resetClock() {
-
+    stopClock()
+    secondsElapsed = 0;
+    setTime()
 }
