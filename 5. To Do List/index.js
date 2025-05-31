9cac0dc2115d345310417108b53d3e5ded393ee4
Aditya <anupama12sg@gmail.com>
2025-05-31 23:09:49 +0530

    chore: added the remove Item

--- 5. To Do List/index.js
+++ 5. To Do List/index.js
@@ -1,19 +1,32 @@
-let items = [];
+let items = ["hello", "1234"];
 
 const itemsDiv = document.getElementById("items")
 
-function loadItems() {
+function renderItems() {
     itemsDiv.innerHTML = null;
 
     for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div")
+        container.style.marginBottom = "10px"
+
         const text = document.createElement("p")
+        text.style.display = "inline";
+        text.style.marginRight = "10px"
         text.textContent = item;
 
-        itemsDiv.appendChild(text)
+        const button = document.createElement("button")
+        button.textContent = "Delete"
+        button.onclick = () => removeItem(idx)
+
+        container.appendChild(text)
+        container.appendChild(button)
+
+        itemsDiv.appendChild(container)
     }
 }
+renderItems()
 
-function renderItems() {
+function loadItems() {
 
 }
 
@@ -25,6 +38,7 @@ function addItem() {
 
 }
 
-function removeItem() {
-
+function removeItem(idx) {
+    items.splice(idx, 1)
+    renderItems()
 }
